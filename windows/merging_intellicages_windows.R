be9b73d907cca00f6ff64e6b6958e73f9aa6daf8
Vinicius Daguano Gastaldi <17752627+vgastaldi@users.noreply.github.com>
2024-04-13 15:34:47 +0200

    Updating the merge script
    Implementing a required change in case experiments were done in different days

--- windows/merging_intellicages_windows.R
+++ windows/merging_intellicages_windows.R
@@ -62,6 +62,81 @@ paste(C3_visits2$VisitID,"_set2",sep = "") -> C3_visits2$VisitID
 read.delim(paste(project_folder2,"\\",project2,"\\","Challenge_3\\Nosepoke.txt",sep="")) -> C3_nosepokes2
 paste(C3_nosepokes2$VisitID,"_set2",sep = "") -> C3_nosepokes2$VisitID
 
+#### In the case the experiments ran separately, it is necessary to adjust the dates to ensure the analysis runs properly ####
+C1_visits2[which(C1_visits2$StartDate == unique(C1_visits2$StartDate)[1]
+),"StartDate"] <- unique(C1_visits$StartDate)[1]
+C1_visits2[which(C1_visits2$StartDate == unique(C1_visits2$StartDate)[2]
+),"StartDate"] <- unique(C1_visits$StartDate)[2]
+C1_visits2[which(C1_visits2$StartDate == unique(C1_visits2$StartDate)[3]
+),"StartDate"] <- unique(C1_visits$StartDate)[3]
+C1_visits2[which(C1_visits2$StartDate == unique(C1_visits2$StartDate)[4]
+),"StartDate"] <- unique(C1_visits$StartDate)[4]
+C1_visits2[which(C1_visits2$StartDate == unique(C1_visits2$StartDate)[5]
+),"StartDate"] <- unique(C1_visits$StartDate)[5]
+
+C2_visits2[which(C2_visits2$StartDate == unique(C2_visits2$StartDate)[1]
+),"StartDate"] <- unique(C2_visits$StartDate)[1]
+C2_visits2[which(C2_visits2$StartDate == unique(C2_visits2$StartDate)[2]
+),"StartDate"] <- unique(C2_visits$StartDate)[2]
+C2_visits2[which(C2_visits2$StartDate == unique(C2_visits2$StartDate)[3]
+),"StartDate"] <- unique(C2_visits$StartDate)[3]
+C2_visits2[which(C2_visits2$StartDate == unique(C2_visits2$StartDate)[4]
+),"StartDate"] <- unique(C2_visits$StartDate)[4]
+C2_visits2[which(C2_visits2$StartDate == unique(C2_visits2$StartDate)[5]
+),"StartDate"] <- unique(C2_visits$StartDate)[5]
+C2_visits2[which(C2_visits2$StartDate == unique(C2_visits2$StartDate)[6]
+),"StartDate"] <- unique(C2_visits$StartDate)[6]
+
+C3_visits2[which(C3_visits2$StartDate == unique(C3_visits2$StartDate)[1]
+),"StartDate"] <- unique(C3_visits$StartDate)[1]
+C3_visits2[which(C3_visits2$StartDate == unique(C3_visits2$StartDate)[2]
+),"StartDate"] <- unique(C3_visits$StartDate)[2]
+C3_visits2[which(C3_visits2$StartDate == unique(C3_visits2$StartDate)[3]
+),"StartDate"] <- unique(C3_visits$StartDate)[3]
+C3_visits2[which(C3_visits2$StartDate == unique(C3_visits2$StartDate)[4]
+),"StartDate"] <- unique(C3_visits$StartDate)[4]
+C3_visits2[which(C3_visits2$StartDate == unique(C3_visits2$StartDate)[5]
+),"StartDate"] <- unique(C3_visits$StartDate)[5]
+C3_visits2[which(C3_visits2$StartDate == unique(C3_visits2$StartDate)[6]
+),"StartDate"] <- unique(C3_visits$StartDate)[6]
+
+C1_nosepokes2[which(C1_nosepokes2$StartDate == unique(C1_nosepokes2$StartDate)[1]
+),"StartDate"] <- unique(C1_nosepokes$StartDate)[1]
+C1_nosepokes2[which(C1_nosepokes2$StartDate == unique(C1_nosepokes2$StartDate)[2]
+),"StartDate"] <- unique(C1_nosepokes$StartDate)[2]
+C1_nosepokes2[which(C1_nosepokes2$StartDate == unique(C1_nosepokes2$StartDate)[3]
+),"StartDate"] <- unique(C1_nosepokes$StartDate)[3]
+C1_nosepokes2[which(C1_nosepokes2$StartDate == unique(C1_nosepokes2$StartDate)[4]
+),"StartDate"] <- unique(C1_nosepokes$StartDate)[4]
+C1_nosepokes2[which(C1_nosepokes2$StartDate == unique(C1_nosepokes2$StartDate)[5]
+),"StartDate"] <- unique(C1_nosepokes$StartDate)[5]
+
+C2_nosepokes2[which(C2_nosepokes2$StartDate == unique(C2_nosepokes2$StartDate)[1]
+),"StartDate"] <- unique(C2_nosepokes$StartDate)[1]
+C2_nosepokes2[which(C2_nosepokes2$StartDate == unique(C2_nosepokes2$StartDate)[2]
+),"StartDate"] <- unique(C2_nosepokes$StartDate)[2]
+C2_nosepokes2[which(C2_nosepokes2$StartDate == unique(C2_nosepokes2$StartDate)[3]
+),"StartDate"] <- unique(C2_nosepokes$StartDate)[3]
+C2_nosepokes2[which(C2_nosepokes2$StartDate == unique(C2_nosepokes2$StartDate)[4]
+),"StartDate"] <- unique(C2_nosepokes$StartDate)[4]
+C2_nosepokes2[which(C2_nosepokes2$StartDate == unique(C2_nosepokes2$StartDate)[5]
+),"StartDate"] <- unique(C2_nosepokes$StartDate)[5]
+C2_nosepokes2[which(C2_nosepokes2$StartDate == unique(C2_nosepokes2$StartDate)[6]
+),"StartDate"] <- unique(C2_nosepokes$StartDate)[6]
+
+C3_nosepokes2[which(C3_nosepokes2$StartDate == unique(C3_nosepokes2$StartDate)[1]
+),"StartDate"] <- unique(C3_nosepokes$StartDate)[1]
+C3_nosepokes2[which(C3_nosepokes2$StartDate == unique(C3_nosepokes2$StartDate)[2]
+),"StartDate"] <- unique(C3_nosepokes$StartDate)[2]
+C3_nosepokes2[which(C3_nosepokes2$StartDate == unique(C3_nosepokes2$StartDate)[3]
+),"StartDate"] <- unique(C3_nosepokes$StartDate)[3]
+C3_nosepokes2[which(C3_nosepokes2$StartDate == unique(C3_nosepokes2$StartDate)[4]
+),"StartDate"] <- unique(C3_nosepokes$StartDate)[4]
+C3_nosepokes2[which(C3_nosepokes2$StartDate == unique(C3_nosepokes2$StartDate)[5]
+),"StartDate"] <- unique(C3_nosepokes$StartDate)[5]
+C3_nosepokes2[which(C3_nosepokes2$StartDate == unique(C3_nosepokes2$StartDate)[6]
+),"StartDate"] <- unique(C3_nosepokes$StartDate)[6]
+
 #### Putting all files together ####
 rbind(C1_visits,C1_visits2) -> C1_visits
 rbind(C2_visits,C2_visits2) -> C2_visits
@@ -71,19 +146,18 @@ rbind(C2_nosepokes,C2_nosepokes2) -> C2_nosepokes
 rbind(C3_nosepokes,C3_nosepokes2) -> C3_nosepokes
 
 #### Writing files ####
-#### Habituation ####
 setwd(project_folder)
 dir.create(project_merged)
 dir.create(paste(project_merged,"\\","Challenge_1\\",sep=""))
 dir.create(paste(project_merged,"\\","Challenge_2\\",sep=""))
 dir.create(paste(project_merged,"\\","Challenge_3\\",sep=""))
 
-#### Habituation ####
+#### Challenge 1 ####
 write.table(C1_visits,paste(project_folder2,"\\",project_merged,"\\","Challenge_1\\Visit.txt",sep=""),col.names = T,row.names = F,quote = F,sep = "\t")
 write.table(C1_nosepokes,paste(project_folder2,"\\",project_merged,"\\","Challenge_1\\Nosepoke.txt",sep=""),col.names = T,row.names = F,quote = F,sep = "\t")
-#### Extinction 1 ####
+#### Challenge 2 ####
 write.table(C2_visits,paste(project_folder2,"\\",project_merged,"\\","Challenge_2\\Visit.txt",sep=""),col.names = T,row.names = F,quote = F,sep = "\t")
 write.table(C2_nosepokes,paste(project_folder2,"\\",project_merged,"\\","Challenge_2\\Nosepoke.txt",sep=""),col.names = T,row.names = F,quote = F,sep = "\t")
-#### Extinction 2 ####
+#### Challenge 3 ####
 write.table(C3_visits,paste(project_folder2,"\\",project_merged,"\\","Challenge_3\\Visit.txt",sep=""),col.names = T,row.names = F,quote = F,sep = "\t")
 write.table(C3_nosepokes,paste(project_folder2,"\\",project_merged,"\\","Challenge_3\\Nosepoke.txt",sep=""),col.names = T,row.names = F,quote = F,sep = "\t")
